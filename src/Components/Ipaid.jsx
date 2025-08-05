2bfa5125a850bebaf74836cde78dd3ea4023b47c
Hammadkhokhar71 <khokharhammad71@gmail.com>
2025-08-05 16:28:56 +0500

    8-5-25

--- src/Components/Ipaid.jsx
+++ src/Components/Ipaid.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import '../css/Ipaid.css';
+import iphoneImage from '../../src/ipone_lpgo.png'; // Adjust the path as necessary
 
 const Ipaid = () => {
   return (
     <div>
       <div className="main-card">
-           <div className='card-2'>
+           <div className='ipaid-card-two'>
           <div class="product-section">
-            <h1>MacBook Air</h1>
-            <p>Sky blue color.</p>
-            <p>Sky high performance with M4.</p>
+            <img src={iphoneImage} alt="" />
+            {/* <p>Apple Watch Series 10.</p> */}
+            <p>Thinstant classic.</p>
             <div class="buttons">
               <a href="#" class="learn-more">Learn more</a>
               <a href="#" class="buy">Buy</a>
             </div>
           </div>
         </div>
-        <div className='card-1'>
+        <div className='ipaid-card-one'>
           <div class="event-section">
-            <p>Apple Worldwide Developers Conference.<br />Join us online June 9–13.</p>
+            <h2 class="card_air-pord">AirPods Pro 2</h2>
+            <p class="card_sub-heading">Now with a Hearing Aid feature.</p>
             <a href="#">Learn more</a>
           </div>
         </div>
--- src/Components/Text.jsx
+++ src/Components/Text.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import '../css/Ipaid.css';
+import iphoneImage from '../../src/ipone_lpgo.png'; // Adjust the path as necessary
 
-function Text() {
+const Ipaid = () => {
   return (
-    <div>Text</div>
+    <div>
+      <div className="main-card">
+           <div className='ipaid-card-two'>
+          <div class="product-section">
+            <img src={iphoneImage} alt="" />
+            {/* <p>Apple Watch Series 10.</p> */}
+            <p>Thinstant classic.</p>
+            <div class="buttons">
+              <a href="#" class="learn-more">Learn more</a>
+              <a href="#" class="buy">Buy</a>
+            </div>
+          </div>
+        </div>
+        <div className='ipaid-card-one'>
+          <div class="event-section">
+            <h2 class="card_air-pord">AirPods Pro 2</h2>
+            <p class="card_sub-heading">Now with a Hearing Aid feature.</p>
+            <a href="#">Learn more</a>
+          </div>
+        </div>
+     
+
+      </div>
+    </div>
   )
 }
 
-export default Text
+export default Ipaid
